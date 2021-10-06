e611bf1a5b2d8352cef0f075638333bd515fc812
Mahmoud Amer <mahmoud.amer@addapptr.com>
2021-10-06 19:59:57 +0200

    Remove cSettings

--- Package.swift
+++ Package.swift
@@ -36,10 +36,6 @@ let package = Package(
                 path: "./Sources/AATKitSPM",
                 resources: [
                     .process("../../Dependencies/AppLovin/Bundle/AppLovinSDKResources.bundle")
-                ],
-                cSettings: [
-                    .define("Other Linker Flags", to: "-ObjC", .when(platforms: [.iOS], configuration: .debug)),
-                    .define("Other Linker Flags", to: "-ObjC", .when(platforms: [.iOS], configuration: .release))
                 ]
                ),
         
@@ -55,10 +51,6 @@ let package = Package(
                 path: "./Sources/AATKitSPM-M1",
                 resources: [
                     .process("../../Dependencies/AppLovin/Bundle/AppLovinSDKResources.bundle")
-                ],
-                cSettings: [
-                    .define("Other Linker Flags", to: "-ObjC", .when(platforms: [.iOS], configuration: .debug)),
-                    .define("Other Linker Flags", to: "-ObjC", .when(platforms: [.iOS], configuration: .release))
                 ]
                ),
         
